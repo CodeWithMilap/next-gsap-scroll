75b1a2b81822f1451b9779d69c765f1a71ef5604
CodeWithMilap <milapdave15@gmail.com>
2023-12-24 18:45:00 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="content-wrapper">
+    {/* Your component content */}
+    <div className="your-text-wrapper-class flex justify-center items-center h-screen">
+      {/* Your text content */}
+      <h1 className="text-vw font-bold text-white whitespace-nowrap text-wrapper transform translate-x-[70%]">
+        A new way of designing, discovering and sharing
+      </h1>
+    </div>
+  </div>
+  )
+}
